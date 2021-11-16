02cb4c6ff34412683f0bf24f11b5457bc177ee96
JinUbuntu <19lyaejin@gmail.com>
2021-11-16 15:53:18 +0900

    Add Hanoi.py

---
+++ Hanoi.py
@@ -0,0 +1,15 @@
+
+def HanoiMove(iNum, iFrom, iTo):
+	if iNum> 1:
+		HanoiMove(iNum- 1, iFrom, 6- iFrom- iTo)
+
+	print(f"Move {iFrom} to {iTo}")
+
+	if iNum> 1:
+		HanoiMove(iNum- 1, 6- iFrom- iTo, iTo)
+
+print("Hanoi Top Program")
+iTop= int(input("Hanoi Top Num: "))
+
+HanoiMove(iNum, 1, 3)
+
